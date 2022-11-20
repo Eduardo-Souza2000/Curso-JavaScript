c35ef44771b941658089a2be6b49aedd9d253a37
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-11-20 00:11:28 -0300

    criando variaveis

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log ("Hello, Java Script");
+
+let primeiroNome ='Eduardo';
+let sobreNome ='Souza';
+
+console.log (primeiroNome);
+console.log (sobreNome);
+
+let nomeCarro = 'Fiat Uno Ano 2000';
+
+console.log (nomeCarro);
+
+const valorCorra = '60';
+console.log (valorCorra);
