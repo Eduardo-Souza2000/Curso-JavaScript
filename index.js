52d0d22b172f9b632e3116be5f8a40041157c6c9
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-11-20 00:58:50 -0300

    Objeto

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
+//Lendo uma paralavra no console do html
 console.log ("Hello, Java Script");
 
+//Declaração de Variavel
+//Variavel -> declatração
 let primeiroNome ='Eduardo';
-let sobreNome ='Souza';
-
+//Leitura da Variavel
 console.log (primeiroNome);
+
+let sobreNome ='Souza';
 console.log (sobreNome);
 
 let nomeCarro = 'Fiat Uno Ano 2000';
-
 console.log (nomeCarro);
 
 const valorCorra = '60';
 console.log (valorCorra);
+
+//MODELOS DE VARIAVEIS PRIMITIVAS
+
+//Variavel de String
+let itemNome = 'String';
+console.log(itemNome);
+
+//Variavel de Numero
+let itemNumero = '1000';
+console.log(itemNumero);
+
+//Variavel de Boleana
+let itemBoleano = true;
+console.log (itemBoleano);
+
+//Variavel Underfined
+let itemColor 
+console.log (itemColor);
+
+//Variavel Nula
+itemNome = null;
+console.log(itemNome);
+
+
+/*
+    Criando um objeto
+    Lembrando que um objeto é um conjunto de informações,
+    referentes a uma determinada coisa
+*/
+
+//EXEMPLO DE OBJETO
+
+let caneta = {
+    nomeItem : 'Caneta',
+    numeroItem : 20,
+    estoqueItem : true,
+    corItem : 'Vermelha'
+}
+
+console.log (caneta);
+
+caneta.corItem = 'branco';
+
+console.log (caneta);
