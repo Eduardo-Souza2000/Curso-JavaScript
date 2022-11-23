e08d81a21e425031bcd24dd1775692740bedfd9e
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-11-23 00:15:22 -0300

    faltando alguns ajustes

--- placar.js
+++ placar.js
@@ -1,17 +1,61 @@
 document.getElementById("campo_botao").value= "ZERAR";
-let campo_numero1 = document.getElementById("numero_1");
-let campo_numero2 = document.getElementById("numero_2");
+let campo_numero1 = document.getElementById("result_1");
+let campo_numero2 = document.getElementById("result_2");
 let campo_botao = document.getElementById("campo_botao");
-let campo_resultado = document.getElementById("resultato");
+let nome1 = document.getElementById("nome_1");
+let nome2 = document.getElementById("nome_2");
+
+campo_numero1.value = 0;
+campo_numero2.value = 0;
 
 
+function nome_time1(){
 
+    if (campo_numero1.value == 0){
 
-function zerar(){
-    campo_numero1.value= "0";
-    campo_numero2.value= "0";
+        nome_1.style.background="green";
+        nome_2.style.background="";
+        nome1.value= window.prompt ('digite o nome time 1');
+    
+    }
+    
+    let resultado1 = Number(campo_numero1.value ++);
+    campo_numero1.innerHTML= resultado1;
+
 }
 
+function nome_time2(){
+
+    if (campo_numero2.value == 0){ 
+        nome_2.style.background="green";
+        nome_1.style.background="";
+        nome2.value= window.prompt ('digite o nome time 2');
+    }
+
+    nome_1.style.background="red";
+    nome_2.style.background="green";
+    let resultado2 = Number(campo_numero2.value ++);
+    campo_numero2.innerHTML= resultado2;
+
+}
+
+function zerar(){
+    Number (campo_numero1.value= 0);
+    campo_numero1.innerHTML= 0;
+    nome1.value= ('');
+    nome_1.style.background="";
+
+    Number (campo_numero2.value= 0);
+    campo_numero2.innerHTML= 0;
+    nome2.value= ('');
+    nome_2.style.background="";
+ }
+
+ 
+if (Number (campo_numero1.value) > Number (campo_numero2.value)){
+    nome_1.style.background="red";
+    
+}
 
 /*
 campo_botao.addEventListener('mouseover',ficar_verde);
