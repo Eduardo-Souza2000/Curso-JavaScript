443da8bcaa3fa0c461220488bbb65287ccedd793
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-11-27 18:18:39 -0300

    desafio 2 FINALIZADO

--- desafio-2/index.js
+++ desafio-2/index.js
@@ -1,1 +1,12 @@
-console.log ('Hi');
+
+let valorProduto = Number (prompt (" Entre com o valor do produto"));
+console.log (valorProduto);
+console.log(typeof(valorProduto));
+
+
+if (valorProduto >= 20){
+    document.getElementById ('resultado').innerHTML = 'APROVADO';
+}
+else {
+    document.getElementById ('resultado').innerHTML = 'NÃO FOI APROVADO';
+}
