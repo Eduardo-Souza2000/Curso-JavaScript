7cf9ceaaa7703d6bf8dd23a074da718732d18a3a
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-11-27 16:46:00 -0300

    factory

--- Object Oriented Programming/index.js
+++ Object Oriented Programming/index.js
@@ -7,9 +7,11 @@ let livroPaginas = 306;
 
 */
 
+/*
 
-//OBJETO
+//OBJETO COM FUNÇÃO DENTRO
 const livro = {
+
     livroTitulo: 'O cortiço',
     livroAutor: 'Manoel de Oliveira',
     livroPaginas: 306,
@@ -17,7 +19,46 @@ const livro = {
         cap1: 'Fundamentos',
         cap2: 'Primeiro',
         cap3: 'terceiro'
+    },
+
+    exibeLivro: function (){
+        console.log ('Printing...')
     }
+ }
+
+    livro.exibeLivro();
+
+
+*/
+
+
+
+/*
+//OBJETOS PARECIDOS  -> CRIANDO UMA FACTORY
+
+
+function criarLivros(titulo, autor, paginas) {
+
+
+
+    const livro = {
+
+        livroTitulo: titulo,
+        livroAutor: autor,
+        livroPaginas: paginas,
+
+        exibeLivro: function (){ 
+            console.log ('Printing...')
+        }
+    }
+
+   return livro;
 }
 
-console.log(livro);
+const livro1 = criarLivros('o cortiço', 'james', 306);
+const livro2 = criarLivros('A cabana', 'Napoleao', 556);
+console.log(livro1);
+console.log(livro2);
+*/
+
+
