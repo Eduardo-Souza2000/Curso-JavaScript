dde55ff264c36dd788d4fbfb2b41c6161d66f6a0
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-11-30 16:52:14 -0300

    exercicio 6 falta finalizar

---
+++ Faculdade/Exercicio-loja/EX_06/index.js
@@ -0,0 +1,48 @@
+let principal = document.getElementById ("principal");
+
+let retanguloLogin = document.createElement("article");
+
+retanguloLogin.innerHTML=`
+
+    <fieldset>
+        <h2>Login</h2>
+    </fieldset>
+
+    <fieldset>
+        <label>Usuário</label><br>
+        <input type="number" name="Usuario" id="login_user" value=""><br>
+
+        <label>Senha</label><br>
+        <input type="number" name="Senha" id="login_senha" value=""><br>
+
+    </fieldset>
+
+    <fieldset class="botao">
+        <input type="submit" value="OK" onclick="comparaLogin()">
+        <input type="submit" value="Limpar" onclick="">
+    </fieldset>
+`
+
+principal.appendChild(retanguloLogin);
+
+function comparaLogin(){
+    
+    let login = document.getElementById ("login_user");
+    let numLogin = Number(login.value);
+    let comparaLogin = 1234;
+
+    let senha = document.getElementById("login_senha");
+    let numSenha = Number(senha.value);
+    let comparaSenha = 9999;
+
+    if (comparaLogin === numLogin){
+        if (numSenha === comparaSenha){
+            window.alert= ("ACESSO PERMITIDO");
+        }else{
+            window.alert= ("SENHA INVALIDA");
+        }
+    } else{
+        window.alert=("LOGIN INVALIDO");
+    }
+
+}
