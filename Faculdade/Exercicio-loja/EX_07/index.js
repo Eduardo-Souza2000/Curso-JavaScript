a8b22e4c1769394fc84717f8ee6c6f0ce8b13ef7
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-12-01 20:13:30 -0300

    novo exercio

--- Faculdade/Exercicio-loja/EX_07/index.js
+++ Faculdade/Exercicio-loja/EX_07/index.js
@@ -1,13 +1,45 @@
 let principal = document.getElementById("principal");
 
-let table = document.getElementById("area_tabela")
-
 
 //FUNÇAO DO BOTAO
 let botAdicionar = document.getElementById ("adicionar");
 botAdicionar.addEventListener ('click', cadastroTabela);
 
+let tabela= document.getElementById("area_tabela");
+let nome = document.getElementById("nome");
+let filhos = document.getElementById("num_filhos");
+let salario = document.getElementById("salario");
+
+
 
 function cadastroTabela(){
+
+    if(nome.value == ("")){
+        alert("NOME NÃO FOI PREENCHIDO");
+        return;
+    }
+
+    if(filhos.value == ("")){
+        alert("NUMERO DE FILHOS NÃO FOI PREENCHIDO");
+        return;
+    }
+
+    if(salario.value === ("")){
+        alert("VALOR DO SALÁRIO NÃO FOI PREENCHIDO");
+        return;
+    }
+
+
+    let criaLinha = document.createElement("tr");
+
+    criaLinha.innerHTML=`
+        <td>${nome.value}</td>
+        <td>${filhos.value}</td>
+        <td>${salario.value}</td>
+    `
+    tabela.appendChild(criaLinha);
     
+    
+
 }
+
