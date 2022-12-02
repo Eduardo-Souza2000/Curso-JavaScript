bb39af3a17038044b80021d2720d137cfd83c925
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-12-02 10:34:36 -0300

    pagina feita com exercicio 7 concluido

--- Faculdade/Exercicio-loja/EX_07/index.js
+++ Faculdade/Exercicio-loja/EX_07/index.js
@@ -5,26 +5,27 @@ let principal = document.getElementById("principal");
 let botAdicionar = document.getElementById ("adicionar");
 botAdicionar.addEventListener ('click', cadastroTabela);
 
+let botAtualizar = document.getElementById ("atualizaDados");
+botAtualizar.addEventListener ('click', calculo);
+
 let tabela= document.getElementById("area_tabela");
 let nome = document.getElementById("nome");
 let filhos = document.getElementById("num_filhos");
 let salario = document.getElementById("salario");
+let exibirResultados = document.getElementById("exibeResultado");
 
-let armazenaDados=[
-
-    {
-        nome:"",
-        numFilhos: 0, 
-        numSalario: 0
-    }
-
-]
-
+let dadosUser=[];
 
 
 
 
 function cadastroTabela(){
+
+    let caputaraDados = {
+        nome:'',
+        filhos: 0,
+        salario:0
+    }
 
     if(nome.value == ("")){
         alert("NOME NÃO FOI PREENCHIDO");
@@ -41,23 +42,64 @@ function cadastroTabela(){
         return;
     }
 
+    caputaraDados.nome= nome.value;
+    caputaraDados.filhos= Number (filhos.value);
+    caputaraDados.salario= Number (salario.value);
+
+    dadosUser.push(caputaraDados);
 
     let criaLinha = document.createElement("tr");
 
     criaLinha.innerHTML=`
-        <td>${nome.value}</td>
-        <td>${filhos.value}</td>
-        <td>R$ ${salario.value}</td>
+        <td>${caputaraDados.nome}</td>
+        <td>${caputaraDados.filhos}</td>
+        <td>R$ ${caputaraDados.salario}</td>
     `
     tabela.appendChild(criaLinha);
 
-    
- 
-    armazenaDados.nome = nome.value;
-    armazenaDados.numFilhos = filhos.value;
-    armazenaDados.numSalario = salario.value
 
-    armazenaDados.push(nome)
+    calculo();
+  
 }
 
 
+function calculo(){
+
+    let mediaSalarial = mediaNumfilhos = maiorSalario = percentualSalario = 0;
+    let somaSalarial = somaFilhos= 0;
+    let quantiPessoas = dadosUser.length;
+    let pessoasRendabaixa = 0;
+    
+    for(i in dadosUser){
+
+        somaSalarial += dadosUser[i].salario;
+        somaFilhos += dadosUser[i].filhos
+
+        if (maiorSalario < dadosUser[i].salario){
+            maiorSalario = dadosUser[i].salario
+        }
+
+        if (dadosUser[i].salario < 1500){
+            ++pessoasRendabaixa
+            
+        }
+
+    }
+
+    mediaSalarial = somaSalarial / quantiPessoas;
+
+    mediaNumfilhos = somaFilhos / quantiPessoas;
+
+    percentualSalario = pessoasRendabaixa / quantiPessoas;
+
+
+
+    exibirResultados.innerHTML= `
+        <p>Média de salário da população: ${mediaSalarial.toFixed(2)}</p>
+        <p>Média do número de filhos:${mediaNumfilhos}</p>
+        <p>Maior salário dos habitantes: ${maiorSalario.toFixed(2)}</p>
+        <p>Percentual de pessoas com salario menor que R$ 1.500,00 : ${percentualSalario.toFixed(2) * 100 } %</p>
+    `
+    
+   
+}
